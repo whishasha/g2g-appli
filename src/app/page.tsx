44a94800af77a6eb5e00dfd85e5d35272774af24
unknown <wilsonma12746@gmail.com>
2025-01-20 16:50:23 +1100

    second edit!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,9 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+            THIS HAS TAKEN ME AN ENTIRE DAY.
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>I HAVE NOT CODED A SINGLE LINE TODAY.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
