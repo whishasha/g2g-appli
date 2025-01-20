a520430860a77be1735cc5a5c051b74180d6d501
unknown <wilsonma12746@gmail.com>
2025-01-20 16:49:24 +1100

    first edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/g2g-appli/next.svg"
-          alt="Next.js logo"
+          src="/g2g-appli/fumo.svg"
+          alt="fumo"
           width={180}
           height={38}
           priority
